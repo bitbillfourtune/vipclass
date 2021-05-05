54e640241d48a3ac7d29a7aa12a664005b2a0ca1
JonatanSouzaBR <jonatan.souza@icloud.com>
2021-05-05 01:13:13 -0300

    add start project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './componentes/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
