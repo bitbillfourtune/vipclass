81fd8f615b6c3d7b6828cf0378670e469d9d3cd0
bitbillfourtune <jonatan.souza@icloud.com>
2021-05-17 00:05:01 -0300

    refactory fase 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  // {
+  //   path: '', component: HomeComponent, children: [
+  //     // { path: '', component: TodayComponent },
+  //     // { path: 'tomorrow', component: TomorrowComponent },
+  //     // { path: 'all', component: AllComponent },
+  //     // { path: 'new', component: NewComponent },
+  //   ],
+  // },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { CardComponent } from './componentes/card/card.component';
+import { CardComponent } from './components/card/card.component';
+import { StartComponent } from './components/start/start.component';
+import { ButtonComponent } from './components/button/button.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { LoginComponent } from './pages/login/login.component';
+
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFireAuthModule } from '@angular/fire/auth';
+// import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardComponent
+    CardComponent,
+    StartComponent,
+    ButtonComponent,
+    UserCardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
