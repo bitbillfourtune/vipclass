e170f2d9a4b7eb7f1f11d4508684186dc31c31ac
John <jonatan.souza@icloud.com>
2021-05-17 23:55:28 -0300

    Refactory default

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
-import { StartComponent } from './components/start/start.component';
 import { ButtonComponent } from './components/button/button.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AddProductComponent } from './pages/add-product/add-product.component';
+import { DefaultComponent } from './pages/default/default.component';
 
 // import { AngularFireModule } from '@angular/fire';
 // import { AngularFireAuthModule } from '@angular/fire/auth';
@@ -22,11 +21,11 @@ import { AddProductComponent } from './pages/add-product/add-product.component';
     AppComponent,
     HomeComponent,
     CardComponent,
-    StartComponent,
     ButtonComponent,
     UserCardComponent,
     LoginComponent,
-    AddProductComponent
+    AddProductComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
