cb81ca2a6b4b7ec7b177faccf177e1156d99fced
John <jonatan.souza@icloud.com>
2021-05-18 23:24:38 -0300

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+import { DefaultComponent } from './pages/default/default.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent },
-  // {
-  //   path: '', component: HomeComponent, children: [
-  //     // { path: '', component: TodayComponent },
-  //     // { path: 'tomorrow', component: TomorrowComponent },
-  //     // { path: 'all', component: AllComponent },
-  //     // { path: 'new', component: NewComponent },
-  //   ],
-  // },
+  {
+    path: '', component: HomeComponent, children: [
+      { path: '', component: DefaultComponent },
+      { path: 'add-product', component: AddProductComponent },
+    ],
+  },
 ];
 
 @NgModule({
